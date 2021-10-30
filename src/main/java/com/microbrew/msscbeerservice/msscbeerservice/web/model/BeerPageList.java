cfe63373dba8b16066b6705ad3a5d9e5c785f134
suddat <suddat.shende@moneyonclick.com>
2021-10-30 14:30:34 +0530

    C-1 : adding web data model. Closes #2

---
+++ src/main/java/com/microbrew/msscbeerservice/msscbeerservice/web/model/BeerPageList.java
@@ -0,0 +1,16 @@
+package com.microbrew.msscbeerservice.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPageList extends PageImpl {
+    public BeerPageList(List content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List content) {
+        super(content);
+    }
+}
