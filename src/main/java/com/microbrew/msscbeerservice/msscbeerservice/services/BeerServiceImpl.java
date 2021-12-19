f5a407af77cac43b6258d54747b44ee57f5dcb67
suddat <suddat.shende@moneyonclick.com>
2021-12-19 17:54:06 +0530

    C-1 : Implement Controller methods along with service implementation Object - closes #8

---
+++ src/main/java/com/microbrew/msscbeerservice/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.microbrew.msscbeerservice.msscbeerservice.services;
+
+import com.microbrew.msscbeerservice.msscbeerservice.domain.Beer;
+import com.microbrew.msscbeerservice.msscbeerservice.respositories.BeerRepository;
+import com.microbrew.msscbeerservice.msscbeerservice.web.controller.NotFoundException;
+import com.microbrew.msscbeerservice.msscbeerservice.web.mapper.BeerMapper;
+import com.microbrew.msscbeerservice.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+
+  private final BeerRepository beerRepository;
+  private final BeerMapper beerMapper;
+
+  @Override
+  public BeerDto getById(UUID beerId) {
+    return beerMapper.beerToBeerDto(
+        beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+  }
+
+  @Override
+  public BeerDto saveNewBeer(BeerDto beerDto) {
+    return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+  }
+
+  @Override
+  public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+    Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+    beer.setBeerName(beerDto.getBeerName());
+    beer.setBeerStyle(beerDto.getBeerStyle().name());
+    beer.setPrice(beerDto.getPrice());
+    beer.setUpc(beerDto.getUpc());
+
+    return beerMapper.beerToBeerDto(beerRepository.save(beer));
+  }
+}
---
+++ src/main/java/com/microbrew/msscbeerservice/msscbeerservice/web/controller/NotFoundException.java
@@ -0,0 +1,3 @@
+package com.microbrew.msscbeerservice.msscbeerservice.web.controller;
+
+public class NotFoundException extends RuntimeException {}
